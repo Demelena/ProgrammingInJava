f253c26cf9db3bd1b1c3d2215ddeaab13beca943
Demelena <demelena@gmail.com>
2018-10-18 14:48:42 -0700

    first update

---
+++ ProgrammingInJava/src/main/java/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args)
+    {System.out.println("Hello, World");
+     System.out.println("Hello");
+     System.out.print("Hi");
+     System.out.println(". How are you?");
+    }
+}
